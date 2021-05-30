2062f27ed2440495b669eead5c9f973c6c0b0102
agenciaradden <contato.radden@gmail.com>
2021-05-30 17:05:39 -0300

    subindo app no git

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', site_key: process.env.SITE_KEY });
+});
+
+
+router.post('/captcha', function(req, res) {
+  
+ if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null)
+  {
+    console.log(req.body)
+    return res.json({"responseError" : req.body});
+  }
+  const secretKey = process.env.SECRET_KEY;
+
+  const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+
+  request(verificationURL,function(error,response,body) {
+    body = JSON.parse(body);
+
+    if(body.success !== undefined && !body.success) {
+      return res.json({"responseError" : "Failed captcha verification"});
+    }
+    res.json({"responseSuccess" : "Sucess"});
+  }); 
+});
+
+module.exports = router;
